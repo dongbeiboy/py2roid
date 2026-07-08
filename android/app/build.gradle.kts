93938d02d44953abd94b16d931db79a1fce46f09
DB-oy <dongbeiboy@users.noreply.github.com>
2026-07-08 22:44:14 +0800

    chore: bump version to 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.xz.py2roid"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
